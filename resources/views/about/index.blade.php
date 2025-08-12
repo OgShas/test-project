c58fc0708ffe5603aa8b9d68428a5ad4f178a739
OgShas <shasog@gmail.com>
2025-08-12 20:13:26 +0300

    some fixes

--- resources/views/about/index.blade.php
+++ resources/views/about/index.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.app')
 
 @section('content')
-    <h1 class="text-center py-3">Current time</h1>
+    <h1 class="text-center py-3">{{$title}}</h1>
     <div class="d-flex justify-content-center align-items-center" style="height: 80vh;">
         <digital-clock></digital-clock>
     </div>
