3d8f6d6ad7c4e379678e839eaef9329ab0a3949d
OgShas <shasog@gmail.com>
2025-08-12 20:15:19 +0300

    some fixes

--- resources/views/about/index.blade.php
+++ resources/views/about/index.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
     <h1 class="text-center py-3">{{$title}}</h1>
-    <div class="d-flex justify-content-center align-items-center" style="height: 80vh;">
+    <div class="d-flex justify-content-center align-items-center">
         <digital-clock></digital-clock>
     </div>
 @endsection
