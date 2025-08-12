feb2839cebb32ad651c633438866f000da2ab994
OgShas <shasog@gmail.com>
2025-08-12 20:06:45 +0300

    done with vue.js

--- resources/views/about/index.blade.php
+++ resources/views/about/index.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.app')
 
 @section('content')
-    <h1>kur</h1>
+    <h1 class="text-center">Current time</h1>
     <div class="d-flex justify-content-center align-items-center" style="height: 80vh;">
         <digital-clock></digital-clock>
     </div>
